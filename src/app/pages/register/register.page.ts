869d90f365566dac8fd09ccb0ac98294594a1e36
bestard <jbestard123@gmail.com>
2020-04-23 01:14:20 -0400

    register

---
+++ src/app/pages/register/register.page.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  register(form:NgForm){
+      /*this.authService.register(form.value.fName, form.value.lName, form.value.email, form.value.password).subscribe(
+      data => {
+        this.authService.login(form.value.email, form.value.password).subscribe(
+          data => {
+          },
+          error => {
+            console.log(error);
+          },
+          () => {
+            this.dismissRegister();
+            this.navCtrl.navigateRoot('/dashboard');
+          }
+        );
+        this.alertService.presentToast(data['message']);
+      },
+      error => {
+        console.log(error);
+      },
+      () => {
+        
+      }
+    );*/
+  }
+}
